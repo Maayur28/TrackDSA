303b8fca8fbb7252fcbe8728f237ffbedc2a879f
Mayur Agarwal <mayuragarwal2812@gmail.com>
2022-11-10 07:56:28 +0530

    added code support for quill

--- src/Components/Notes/notes.jsx
+++ src/Components/Notes/notes.jsx
@@ -21,6 +21,7 @@ import {
 } from "@ant-design/icons";
 import Cookies from "js-cookie";
 import { useNavigate } from "react-router-dom";
+import EditorToolbar, { modules, formats } from "./EditorToolbar";
 
 const { Panel } = Collapse;
 
@@ -201,6 +202,7 @@ const Notes = () => {
           delete values.__v;
           obj.userid = data.userid;
           obj.notes = [];
+          values.note = note;
           obj.notes.push(values);
           fetch("https://trackdsaproblems.herokuapp.com/addnote", {
             method: "POST",
@@ -312,6 +314,37 @@ const Notes = () => {
     }
   };
 
+  const handleNoteChange = (value) => {
+    setNote(value);
+  };
+
+  const CustomUndo = () => (
+    <svg viewBox="0 0 18 18">
+      <polygon className="ql-fill ql-stroke" points="6 10 4 12 2 10 6 10" />
+      <path
+        className="ql-stroke"
+        d="M8.09,13.91A4.6,4.6,0,0,0,9,14,5,5,0,1,0,4,9"
+      />
+    </svg>
+  );
+
+  const CustomRedo = () => (
+    <svg viewBox="0 0 18 18">
+      <polygon className="ql-fill ql-stroke" points="12 10 14 12 16 10 12 10" />
+      <path
+        className="ql-stroke"
+        d="M9.91,13.91A4.6,4.6,0,0,1,9,14a5,5,0,1,1,5-5"
+      />
+    </svg>
+  );
+
+  function undoChange() {
+    this.quill.history.undo();
+  }
+  function redoChange() {
+    this.quill.history.redo();
+  }
+
   return (
     <>
       <div className="notes-div">
@@ -363,14 +396,86 @@ const Notes = () => {
                     </Space>
                   }
                 >
-                  <div>
-                    {
-                      <ReactQuill
-                        theme="snow"
-                        value={note}
-                        onChange={setNote}
-                      />
-                    }
+                  <div className="text-editor">
+                    <div className={`toolbar quill${index}`}>
+                      <span className="ql-formats">
+                        <select className="ql-font" defaultValue="arial">
+                          <option value="arial">Arial</option>
+                          <option value="comic-sans">Comic Sans</option>
+                          <option value="courier-new">Courier New</option>
+                          <option value="georgia">Georgia</option>
+                          <option value="helvetica">Helvetica</option>
+                          <option value="lucida">Lucida</option>
+                        </select>
+                        <select className="ql-size" defaultValue="medium">
+                          <option value="extra-small">Size 1</option>
+                          <option value="small">Size 2</option>
+                          <option value="medium">Size 3</option>
+                          <option value="large">Size 4</option>
+                        </select>
+                        <select className="ql-header" defaultValue="3">
+                          <option value="1">Heading</option>
+                          <option value="2">Subheading</option>
+                          <option value="3">Normal</option>
+                        </select>
+                      </span>
+                      <span className="ql-formats">
+                        <button className="ql-bold" />
+                        <button className="ql-italic" />
+                        <button className="ql-underline" />
+                        <button className="ql-strike" />
+                      </span>
+                      <span className="ql-formats">
+                        <button className="ql-list" value="ordered" />
+                        <button className="ql-list" value="bullet" />
+                        <button className="ql-indent" value="-1" />
+                        <button className="ql-indent" value="+1" />
+                      </span>
+                      <span className="ql-formats">
+                        <button className="ql-script" value="super" />
+                        <button className="ql-script" value="sub" />
+                        <button className="ql-blockquote" />
+                        <button className="ql-direction" />
+                      </span>
+                      <span className="ql-formats">
+                        <select className="ql-align" />
+                        <select className="ql-color" />
+                        <select className="ql-background" />
+                      </span>
+                      <span className="ql-formats">
+                        <button className="ql-link" />
+                        <button className="ql-image" />
+                        <button className="ql-video" />
+                      </span>
+                      <span className="ql-formats">
+                        <button className="ql-formula" />
+                        <button className="ql-code-block" />
+                        <button className="ql-clean" />
+                      </span>
+                      <span className="ql-formats">
+                        <button className="ql-undo">
+                          <CustomUndo />
+                        </button>
+                        <button className="ql-redo">
+                          <CustomRedo />
+                        </button>
+                      </span>
+                    </div>
+                    <ReactQuill
+                      theme="snow"
+                      value={note}
+                      onChange={handleNoteChange}
+                      placeholder={"Write something..."}
+                      modules={{
+                        toolbar: { container: `.quill${index}` },
+                        history: {
+                          delay: 500,
+                          maxStack: 100,
+                          userOnly: true,
+                        },
+                      }}
+                      formats={formats}
+                    />
                   </div>
                 </Panel>
               ))}
@@ -414,7 +519,17 @@ const Notes = () => {
             <Input placeholder="Exp: Two sum" />
           </Form.Item>
           <Form.Item name="note" label="Note">
-            <ReactQuill theme="snow" value={note} onChange={setNote} />
+            <div className="text-editor">
+              <EditorToolbar />
+              <ReactQuill
+                theme="snow"
+                value={note}
+                onChange={handleNoteChange}
+                placeholder={"Write something..."}
+                modules={modules}
+                formats={formats}
+              />
+            </div>
           </Form.Item>
           <Form.Item>
             <Space>
